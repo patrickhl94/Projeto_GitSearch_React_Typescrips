a0a762f047b88084b5dcb872553202e701ad4ec8
Patrick Lima <patrickhl94@hotmail.com>
2020-05-04 23:49:24 -0300

    Upadated App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return <h1>Olá mundo!</h1>;
-}
+import Routes from './router/index';
+import GlobalStyle from './styles/global';
+
+const App: React.FC = () => (
+  <>
+    <Routes />
+    <GlobalStyle />
+  </>
+);
 
 export default App;
